b2abfbc8ee4d1d12ffb6c21e6a9dea35bd4ea444
Imad A. Bakir <imad.bakir@edigitpath.com>
2019-03-25 00:00:41 +0300

    disabled sorting for Details column, disabled clear sort

--- src/app/my-requests/my-requests.component.ts
+++ src/app/my-requests/my-requests.component.ts
@@ -29,7 +29,7 @@ export class MyRequestsComponent {
     requestName: { name: 'Request Name', sortable: true },
     requestDate: { name: 'Request Date', sortable: true, formatDate: true },
     status: { name: 'Status', sortable: true },
-    data: { name: 'Details', sortable: true, display: 'detailsButton', param1: 'link', param2: 'data', param3: 'cmmnId' }
+    data: { name: 'Details', sortable: false, display: 'detailsButton', param1: 'link', param2: 'data', param3: 'cmmnId' }
 
   };
 
