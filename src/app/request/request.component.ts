7a00b09bdc233524f17e44f94e50e8ca1ebe3558
Imad A. Bakir <imad.bakir@edigitpath.com>
2019-06-16 17:19:18 +0300

    request mapping

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -26,10 +26,45 @@ export class RequestComponent implements OnInit {
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
-      this.serviceId = params['serviceId'];
+      /*
+      5d0429e530313600190003de - 1
+
+5d0429e830313600170001ee - 2
+
+
+5d0429f130313600120000da - 3
+دعم تجهيزات مشروع
+
+5d0429f730313600110000a9 - 4 رسوم خدمات استشاريه
+
+
+5d0429fb3031360013000249 - 5
+د
+       */
+      switch (params['serviceId']) {
+        case '5d0429e530313600190003de':
+          this.serviceId = 1;
+          break;
+        case '5d0429e830313600170001ee':
+          this.serviceId = 2;
+          break;
+        case '5d0429f130313600120000da':
+          this.serviceId = 3;
+          break;
+        case '5d0429f730313600110000a9':
+          this.serviceId = 4;
+          break;
+        case '5d0429fb3031360013000249':
+          this.serviceId = 5;
+          break;
+      }
       console.log(this.route.snapshot);
       this.params = [
-        { url: environment.beneficiaryApi.api, success: `submission.data = {requesterInfo: {data: response}};` }
+        // { url: environment.beneficiaryApi.api, success: `submission.data = {requesterInfo: {data: response}};` }
+        {
+          url: environment.beneficiaryApi.api,
+          success: `submission.data = {entrepreneurshipType:"${this.serviceId}",serviceId:"${this.serviceId}",requesterInfo: {data: response}};`
+        }
       ];
       // this.params.success = `submission.data = {serviceId:"${this.serviceId}",requesterInfo: {data: response}};`;
       // @TODO: pass ServiceId
