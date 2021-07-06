c1fb8c7469c54b2c97f7bd8d707823233d5d3d11
TaherKhateep <t.khateep@diwink.com>
2021-07-06 14:52:38 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Injector } from '@angular/core';
+import { Injector, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { BaseComponent } from './shared/components/base.component';
@@ -8,11 +8,53 @@ import { BaseComponent } from './shared/components/base.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent extends BaseComponent {
+export class AppComponent extends BaseComponent implements OnInit {
+  style = 'locale-style';
+  isStylesLoaded = false;
+  isFontsLoaded = false;
 
   constructor(public injector: Injector,
     public platform: Platform) {
     super(injector);
+  }
+
+  ngOnInit() {
+    // const fontName = this.configurationService.getConfig().fontName;
+    // const headerColor = this.configurationService.getConfig().headerColor;
+    // const bodyColor = this.configurationService.getConfig().bodyColor;
+    // const footerColor = this.configurationService.getConfig().footerColor;
+    // const textColor = this.configurationService.getConfig().textColor;
+    // const style = this.configurationService.getConfig().style;
+
+    const stylesheet = document.createElement('link');
+
+    stylesheet.addEventListener('load', () => {
+      // document.documentElement.style.setProperty('--font-name', fontName);
+      // document.documentElement.style.setProperty('--header-color', headerColor);
+      // document.documentElement.style.setProperty('--body-color', bodyColor);
+      // document.documentElement.style.setProperty('--footer-color', footerColor);
+      // document.documentElement.style.setProperty('--text-color', textColor);
+      this.isStylesLoaded = true;
+    });
+
+    stylesheet.rel = 'stylesheet';
+    stylesheet.href = this.style + '.css';
+    document.getElementsByTagName('head')[0].appendChild(stylesheet);
+
+
+    // const customStylesheet = document.createElement('link');
+    // customStylesheet.rel = 'stylesheet';
+    // customStylesheet.href = 'assets/custom.css';
+    // document.getElementsByTagName('head')[0].appendChild(customStylesheet);
+
+    // WebFontLoader.load({
+    //   active: () => {
+    //     this.isFontsLoaded = true;
+    //   },
+    //   google: {
+    //     families: [fontName]
+    //   }
+    // });
   }
 
 }
