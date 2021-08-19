1a0755599a343a92f58732134fa51e9e652b52d6
Imad A. Bakir <imad.bakir@edigitpath.com>
2021-08-19 11:36:54 +0300

    include formio from npm

---
+++ src/app/resource-wrapper.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResourcesModule } from 'dp-formio';
+
+@NgModule({
+  declarations: [],
+  imports: [ ResourcesModule ],
+  exports: [],
+  providers: [],
+})
+export class ResourceWrapperModule {}
