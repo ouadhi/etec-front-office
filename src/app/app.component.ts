8a193b55bec9226adad43da9b2258fd546f3ad23
TaherKhateep <t.khateep@diwink.com>
2021-09-21 10:13:05 +0300

    update register custom components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { registerRatingComponent } from 'src/formio/src/public_api';
 import { Injector, OnInit, ViewEncapsulation, Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { BaseComponent } from './shared/components/base.component';
@@ -6,9 +5,9 @@ import { BaseComponent } from './shared/components/base.component';
 import { registerTemplateComponent } from 'src/formio/src/public_api';
 import { registerTableTreeComponent } from 'src/formio/src/public_api';
 import { registerAssessmentComponent } from 'src/formio/src/public_api';
-import { registerProccessRequirmentsComponent } from 'src/formio/src/lib/custom-component/components/proccess-requirments-wrapper/proccess-requirments-wrapper.formio';
+import { registerProccessRequirmentsComponent } from 'src/formio/src/lib/features/formio/custom-component/components/proccess-requirments-wrapper/proccess-requirments-wrapper.formio';
 // import { registerProccessRequirmentsComponent } from 'src/formio/src/public_api';
-import { registerMapComponent } from 'src/formio/src/lib/custom-component/components/map-wrapper/map-wrapper.formio';
+import { registerMapComponent } from 'src/formio/src/lib/features/formio/custom-component/components/map-wrapper/map-wrapper.formio';
 // import { registerMapComponent } from 'src/formio/src/public_api';
 
 @Component({
@@ -22,7 +21,6 @@ export class AppComponent extends BaseComponent implements OnInit {
   constructor(public injector: Injector,
     public platform: Platform) {
     super(injector);
-    registerRatingComponent(injector);
     registerTableTreeComponent(injector);
     registerAssessmentComponent(injector);
     registerTemplateComponent(injector);
