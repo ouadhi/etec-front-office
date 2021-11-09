ef01a64a98ea10ca7fde5df12aa5b1e22646b53a
Mostafa Kamal Mohamed <m.helal@comptechco.com>
2021-11-09 17:43:13 +0300

    Add Hijri Date Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,45 @@
-import { Injector, OnInit, ViewEncapsulation, Component } from '@angular/core';
-import { Platform } from '@ionic/angular';
-import { BaseComponent } from './shared/components/base.component';
+import { Injector, OnInit, ViewEncapsulation, Component } from "@angular/core";
+import { Platform } from "@ionic/angular";
+import { BaseComponent } from "./shared/components/base.component";
 
-import { registerTemplateComponent } from 'src/formio/src/public_api';
-import { registerTableTreeComponent } from 'src/formio/src/public_api';
-import { registerAssessmentComponent } from 'src/formio/src/public_api';
-import { registerProccessRequirmentsComponent } from 'src/formio/src/lib/features/formio/custom-component/components/proccess-requirments-wrapper/proccess-requirments-wrapper.formio';
+import {
+  registerHijriDateComponent,
+  registerTemplateComponent,
+} from "src/formio/src/public_api";
+import { registerTableTreeComponent } from "src/formio/src/public_api";
+import { registerAssessmentComponent } from "src/formio/src/public_api";
+import { registerProccessRequirmentsComponent } from "src/formio/src/lib/features/formio/custom-component/components/proccess-requirments-wrapper/proccess-requirments-wrapper.formio";
 // import { registerProccessRequirmentsComponent } from 'src/formio/src/public_api';
-import { registerMapComponent } from 'src/formio/src/lib/features/formio/custom-component/components/map-wrapper/map-wrapper.formio';
+import { registerMapComponent } from "src/formio/src/lib/features/formio/custom-component/components/map-wrapper/map-wrapper.formio";
 // import { registerMapComponent } from 'src/formio/src/public_api';
-import { registerIndicatorsComponent } from 'src/formio/src/lib/features/formio/custom-component/components/indicators-wrapper/indicators-wrappe.formio';
+import { registerIndicatorsComponent } from "src/formio/src/lib/features/formio/custom-component/components/indicators-wrapper/indicators-wrappe.formio";
 // import { registerIndicatorsComponent } from 'src/formio/src/public_api';
-import { NavigationStart } from '@angular/router';
+import { NavigationStart } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   // styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent extends BaseComponent implements OnInit {
   fullPage = false;
 
-  constructor(public injector: Injector,
-    public platform: Platform) {
+  constructor(public injector: Injector, public platform: Platform) {
     super(injector);
     registerTableTreeComponent(injector);
     registerAssessmentComponent(injector);
     registerTemplateComponent(injector);
     registerProccessRequirmentsComponent(injector);
+    registerHijriDateComponent(injector);
     registerMapComponent(injector);
     registerIndicatorsComponent(injector);
-    this.sub = this.router.events.subscribe(event => {
+    this.sub = this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
-        this.fullPage = event.url.includes('reset-password');
+        this.fullPage = event.url.includes("reset-password");
       }
     });
   }
 
-  ngOnInit() {
-
-  }
-
+  ngOnInit() {}
 }
