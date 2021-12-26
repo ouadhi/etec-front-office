a6201eb5aa008bf668b90eaa972d1d5b74b1b16f
m.helal <m.helal@comptechco.com>
2021-12-26 09:14:49 +0300

    Adding Redirection Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,54 @@
-import { Injector, OnInit, ViewEncapsulation, Component } from "@angular/core";
-import { Platform } from "@ionic/angular";
-import { BaseComponent } from "./shared/components/base.component";
+import { Injector, OnInit, ViewEncapsulation, Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { BaseComponent } from './shared/components/base.component';
 
 import {
-  registerAssessmentComponent,
-  registerEditgridResourceComponent,
-  registerFormKeyWrapperComponent,
-  registerHijriDateComponent,
-  registerIndicatorsComponent,
-  registerMapComponent,
-  registerMasterDetailsResourceComponent,
-  registerProccessRequirmentsComponent,
-  registerTableTreeComponent,
-  registerTemplateComponent,
-  registerGroupedTableComponent,
-  registerStepperComponent,
-} from "src/formio/src/public_api";
+	registerAssessmentComponent,
+	registerEditgridResourceComponent,
+	registerFormKeyWrapperComponent,
+	registerHijriDateComponent,
+	registerIndicatorsComponent,
+	registerMapComponent,
+	registerMasterDetailsResourceComponent,
+	registerProccessRequirmentsComponent,
+	registerTableTreeComponent,
+	registerTemplateComponent,
+	registerGroupedTableComponent,
+	registerStepperComponent,
+	registerRedirectionButtonComponent,
+} from 'src/formio/src/public_api';
 
-import { NavigationStart } from "@angular/router";
-
+import { NavigationStart } from '@angular/router';
 @Component({
-  selector: "app-root",
-  templateUrl: "./app.component.html",
-  // styleUrls: ['./app.component.scss'],
-  encapsulation: ViewEncapsulation.None,
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	// styleUrls: ['./app.component.scss'],
+	encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent extends BaseComponent implements OnInit {
-  fullPage = false;
+	fullPage = false;
 
-  constructor(public injector: Injector, public platform: Platform) {
-    super(injector);
-    registerTableTreeComponent(injector);
-    registerAssessmentComponent(injector);
-    registerTemplateComponent(injector);
-    registerMasterDetailsResourceComponent(injector);
-    registerProccessRequirmentsComponent(injector);
-    registerMapComponent(injector);
-    registerIndicatorsComponent(injector);
-    registerHijriDateComponent(injector);
-    registerFormKeyWrapperComponent(injector);
-    registerEditgridResourceComponent(injector);
-    registerGroupedTableComponent(injector);
-    registerStepperComponent(injector);
-    this.sub = this.router.events.subscribe(event => {
-      if (event instanceof NavigationStart) {
-        this.fullPage = event.url.includes("reset-password");
-      }
-    });
-  }
+	constructor(public injector: Injector, public platform: Platform) {
+		super(injector);
+		registerTableTreeComponent(injector);
+		registerAssessmentComponent(injector);
+		registerTemplateComponent(injector);
+		registerMasterDetailsResourceComponent(injector);
+		registerProccessRequirmentsComponent(injector);
+		registerMapComponent(injector);
+		registerIndicatorsComponent(injector);
+		registerHijriDateComponent(injector);
+		registerFormKeyWrapperComponent(injector);
+		registerEditgridResourceComponent(injector);
+		registerGroupedTableComponent(injector);
+		registerStepperComponent(injector);
+		registerRedirectionButtonComponent(injector);
+		this.sub = this.router.events.subscribe((event) => {
+			if (event instanceof NavigationStart) {
+				this.fullPage = event.url.includes('reset-password');
+			}
+		});
+	}
 
-  ngOnInit() {}
+	ngOnInit() {}
 }
--- src/app/modules/requests/request-task/request-task.component.ts
+++ src/app/modules/requests/request-task/request-task.component.ts
@@ -12,125 +12,131 @@ declare var $: any;
  * Main Task Component
  */
 @Component({
-  selector: 'app-request-task',
-  templateUrl: './request-task.component.html',
-  // styleUrls: ['./request-task.component.scss'],
-  encapsulation: ViewEncapsulation.None
-
+	selector: 'app-request-task',
+	templateUrl: './request-task.component.html',
+	// styleUrls: ['./request-task.component.scss'],
+	encapsulation: ViewEncapsulation.None,
 })
 export class RequestTaskComponent extends BaseComponent implements OnInit {
-  @ViewChild(FormioComponent) formComponent: FormioComponent;
+	@ViewChild(FormioComponent) formComponent: FormioComponent;
 
-  updateTask = 'SERVICE.beneficiaryTask'
-  proccessName = '';
+	updateTask = 'SERVICE.beneficiaryTask';
+	proccessName = '';
 
-  task: any = {};
-  submission: any = {};
-  public max = 65;
-  public mainWidth = 65;
-  public seperator = true;
-  public showComments = true;
-  public showCommentsPopup = false;
-  form = {
-    formKey: '',
-    readOnly: false,
-    version: {},
-    ready: false
-  };
-  params;
-  /*
+	task: any = {};
+	submission: any = {};
+	public max = 65;
+	public mainWidth = 65;
+	public seperator = true;
+	public showComments = true;
+	public showCommentsPopup = false;
+	form = {
+		formKey: '',
+		readOnly: false,
+		version: {},
+		ready: false,
+	};
+	params;
+	/*
   submitPromise: Promise<any>;
   submitPromiseResolve: any;
 */
 
-  constructor(public injector: Injector,
-    public modalController: ModalController,
-    public etecService: ETECService,
-    public caseActivity: CaseActivityService) { super(injector); }
+	constructor(
+		public injector: Injector,
+		public modalController: ModalController,
+		public etecService: ETECService,
+		public caseActivity: CaseActivityService
+	) {
+		super(injector);
+	}
 
-  /**
-   * Go Back After Task is completed and return navigation promise
-   */
-  goBack() {
-    /* return this.router.navigate(['tasks',
+	/**
+	 * Go Back After Task is completed and return navigation promise
+	 */
+	goBack() {
+		/* return this.router.navigate(['tasks',
        ...(this.route.parent.snapshot.params.filterId ? [this.route.parent.snapshot.params.filterId] : [])]);*/
-    this.location.back();
-  }
+		this.location.back();
+	}
 
+	/**
+	 * On Form Submit Event Callback
+	 *  @param submission
+	 *  Submission Object
+	 */
+	onSubmit(submission) {
+		this.toastrService.show(
+			this.translateService.instant('SERVICE.BENEFICIARY_TASK_SUCCESS'),
+			this.translateService.instant('OperationDone'),
+			{
+				toastClass: 'notification-toast',
+				closeButton: true,
+				enableHtml: true,
+				toastComponent: SuccessToast,
+			}
+		);
+		this.goBack();
+	}
 
-  /**
-   * On Form Submit Event Callback
-   *  @param submission
-   *  Submission Object
-   */
-  onSubmit(submission) {
-    this.toastrService.show(
-      this.translateService.instant('SERVICE.BENEFICIARY_TASK_SUCCESS'),
-      this.translateService.instant('OperationDone'),
-      {
-        toastClass: 'notification-toast',
-        closeButton: true,
-        enableHtml: true,
-        toastComponent: SuccessToast
-      });
-    this.goBack();
-  }
+	customSubmit(submission) {
+		const send = Object.assign({}, this.formComponent.submission);
+		send.data.saveAndSend = true;
+		this.formComponent.submitForm(send);
+	}
 
-  customSubmit(submission) {
-    const send = Object.assign({}, this.formComponent.submission);
-    send.data.saveAndSend = true;
-    this.formComponent.submitForm(send);
-  }
+	formLoad(formSettings) {
+		setTimeout(() => {
+			if (formSettings.properties && eval(formSettings.properties.hideSubmit)) {
+				document.getElementsByName('data[submit]')[0].style.display = 'none';
+				document.getElementById('custom-actions').style.display = 'block';
+			}
 
-  formLoad(formSettings) {
-    setTimeout(() => {
-      if (formSettings.properties && eval(formSettings.properties.hideSubmit)) {
-        document.getElementsByName('data[submit]')[0].style.display = 'none';
-        document.getElementById('custom-actions').style.display = 'block';
-      }
+			$('.input-group:has(.input-group-addon.input-group-prepend)').css('display', 'block');
+		});
+	}
 
-      $('.input-group:has(.input-group-addon.input-group-prepend)').css('display', 'block');
-    })
-  }
+	/**
+	 * ngOnInit: on init subscribe to route changes
+	 */
+	ngOnInit() {
+		this.sub = this.route.params.subscribe((params) => {
+			this.sub = this.caseActivity.getRequestTask(params.taskId).subscribe(async (data) => {
+				if (data.formKey) {
+					const processDefinitionId = data.processDefinitionId.split(':')[0];
+					const requestName = `${processDefinitionId}`;
+					const taskName = `taskTitle.${data.taskDefinitionKey}`;
+					this.sub = this.translateService
+						.get([requestName, taskName, this.updateTask])
+						.subscribe((keys) => {
+							if (keys[requestName] != requestName && keys[taskName] != taskName) {
+								this.proccessName = `${keys[requestName]} - ${keys[taskName]}`;
+							} else {
+								this.proccessName = keys[this.updateTask];
+							}
+						});
 
+					this.form.ready = false;
 
+					const etecData = await this.etecService.getTaskData();
+					this.submission.data = { ...etecData };
+					this.params = [
+						{
+							url: `${environment.gateway}${environment.endpoints.humanTask}${data.formKey.replace(
+								'{caseId}',
+								params.caseId
+							)}`,
+							parallel: false,
+							success: `submission.data = {...response.data,...submission.data, taskId:"${params.taskId}", requestId:"${params.requestId}"};`,
+						},
+					];
+					this.form.formKey = data.formKey.split('/')[2];
 
-  /**
-   * ngOnInit: on init subscribe to route changes
-   */
-  ngOnInit() {
-    this.sub = this.route.params.subscribe(params => {
-      this.sub = this.caseActivity.getRequestTask(params.taskId).subscribe(async data => {
-        if (data.formKey) {
-          const processDefinitionId = data.processDefinitionId.split(':')[0];
-          const requestName = `${processDefinitionId}`;
-          const taskName = `taskTitle.${data.taskDefinitionKey}`;
-          this.sub = this.translateService.get([requestName, taskName, this.updateTask])
-            .subscribe(keys => {
-              if (keys[requestName] != requestName && keys[taskName] != taskName) {
-                this.proccessName = `${keys[requestName]} - ${keys[taskName]}`;
-              } else {
-                this.proccessName = keys[this.updateTask];
-              }
-            });
-
-          this.form.ready = false;
-
-          const etecData = await this.etecService.getTaskData();
-          this.submission.data = { ...etecData };
-          this.params = [{
-            url: `${environment.gateway}${environment.endpoints.humanTask}${data.formKey.replace('{caseId}', params.caseId)}`,
-            parallel: false,
-            success: `submission.data = {...response.data,...submission.data, taskId:"${params.taskId}", requestId:"${params.requestId}"};`
-          }];
-          this.form.formKey = data.formKey.split('/')[2];
-
-          setTimeout(() => { this.form.ready = true; }, 0);
-        }
-      });
-
-    });
-  }
-
-
+					setTimeout(() => {
+						this.form.ready = true;
+					}, 0);
+				}
+			});
+		});
+	}
 }
