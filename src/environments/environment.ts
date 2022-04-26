218965ad3cb63ed3203cbdbb160ecce9e0695df5
m.helal <m.helal@comptechco.com>
2022-04-26 11:41:58 +0300

    Adding Eligibility Verification Report Api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,61 +3,62 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  gateway: 'http://etec-bo.comptechco.com:80',
-  cms: 'http://etec-fo.comptechco.com:8089',
-  appConfig: {
-    endpoint: '/api/collections/get/AppConfig1/',
-    id: '6069bbe7666434004e00012d',
-    frontOfficeSettings: 'api/singletons/get/frontOfficeSettings',
-    formioEndpoint: '/api/collections/get/formioConfig/',
-    formioId: '612bc06a5d4cda7713720b52',
-    formioSettings: 'api/singletons/get/formioSettings'
-  },
-  keycloak: {
-    url: 'http://etec-bo.comptechco.com:9080/auth/',
-    realm: 'etec',
-    clientId: 'fo-web'
-  },
-  formio: {
-    apiUrl: "https://api.form.io/",
-    appUrl: 'http://etec-fo.comptechco.com:8084/ksuqnygnhgztelf/'
-  },
-  endpoints: {
-    tasks: '/requestmanagement/api/portal/tasks',
-    anonymousTasks: '/requestmanagement/api/portal/tasks/anonymous',
-    camundaTask: '/requestmanagement/rest/task',
-    myRequests: '/requestmanagement/api/portal/requests',
-    myBeneficiarySegments: '/requestmanagement/api/portal/beneficiary-segments-cms',
-    caseActivity: '/requestmanagement/rest/history/case-activity-instance',
-    caseActivityDetails: '/requestmanagement/rest/history/detail',
-    count: '/requestmanagement/api/requests/count',
-    statuses: '/requestmanagement/api/portal/eservices',
-    beneficiaryApi: '/dataservice/api/portal/beneficiaries',
-    deparments: '/referencedata/api/portal/departments',
-    services: '/referencedata/api/request/eservices',
-    servicesNew: '/requestmanagement/api/portal/eservices',
-    filterStatuses: '/referencedata/departments',
-    request: '/referencedata/caseNames',
-    account: '/api/account',
-    humanTask: '/dataservice/api/tasks/case/',
-    notifications: '/notification/api/',
-    socket: '/notification/websocket/orchestra',
-    dataservice:"/dataservice/api/"
-  },
-  roles: {
-    beneficiary: 'ROLE_BENEFICIARY',
-    branch_specialist: 'ROLE_SERVICE_SPECIALIST_BRANCH',
-    department_specialist: 'ROLE_DEPARTMENT_ENABLEMENT_SPECIALIST',
-    admin: 'ROLE_ADMIN'
-  },
-  serviceCatalogControl:{
-    showCarousel : false,
-    showHasTask : true,
-    showMostUsedService : true,
-    showCatalog : false
-  },
-  pdftronLicenceKey: ''
+	production: false,
+	gateway: 'http://etec-bo.comptechco.com:80',
+	cms: 'http://etec-fo.comptechco.com:8089',
+	appConfig: {
+		endpoint: '/api/collections/get/AppConfig1/',
+		id: '6069bbe7666434004e00012d',
+		frontOfficeSettings: 'api/singletons/get/frontOfficeSettings',
+		formioEndpoint: '/api/collections/get/formioConfig/',
+		formioId: '612bc06a5d4cda7713720b52',
+		formioSettings: 'api/singletons/get/formioSettings',
+	},
+	keycloak: {
+		url: 'http://etec-bo.comptechco.com:9080/auth/',
+		realm: 'etec',
+		clientId: 'fo-web',
+	},
+	formio: {
+		apiUrl: 'https://api.form.io/',
+		appUrl: 'http://etec-fo.comptechco.com:8084/ksuqnygnhgztelf/',
+	},
+	endpoints: {
+		tasks: '/requestmanagement/api/portal/tasks',
+		anonymousTasks: '/requestmanagement/api/portal/tasks/anonymous',
+		camundaTask: '/requestmanagement/rest/task',
+		myRequests: '/requestmanagement/api/portal/requests',
+		myBeneficiarySegments: '/requestmanagement/api/portal/beneficiary-segments-cms',
+		caseActivity: '/requestmanagement/rest/history/case-activity-instance',
+		caseActivityDetails: '/requestmanagement/rest/history/detail',
+		count: '/requestmanagement/api/requests/count',
+		statuses: '/requestmanagement/api/portal/eservices',
+		beneficiaryApi: '/dataservice/api/portal/beneficiaries',
+		deparments: '/referencedata/api/portal/departments',
+		services: '/referencedata/api/request/eservices',
+		servicesNew: '/requestmanagement/api/portal/eservices',
+		filterStatuses: '/referencedata/departments',
+		request: '/referencedata/caseNames',
+		account: '/api/account',
+		humanTask: '/dataservice/api/tasks/case/',
+		notifications: '/notification/api/',
+		socket: '/notification/websocket/orchestra',
+		dataservice: '/dataservice/api/',
+		eligibilityVerificationReportApi: '/reportingservice/api/requirements',
+	},
+	roles: {
+		beneficiary: 'ROLE_BENEFICIARY',
+		branch_specialist: 'ROLE_SERVICE_SPECIALIST_BRANCH',
+		department_specialist: 'ROLE_DEPARTMENT_ENABLEMENT_SPECIALIST',
+		admin: 'ROLE_ADMIN',
+	},
+	serviceCatalogControl: {
+		showCarousel: false,
+		showHasTask: true,
+		showMostUsedService: true,
+		showCatalog: false,
+	},
+	pdftronLicenceKey: '',
 };
 
 /*
