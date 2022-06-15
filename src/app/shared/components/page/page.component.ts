1a43eea3b281f6fb817d780809c4ac055e1b8d49
m.helal <m.helal@comptechco.com>
2022-06-15 13:48:48 +0300

    Scrolling up when open service-catalog page

--- src/app/shared/components/page/page.component.ts
+++ src/app/shared/components/page/page.component.ts
@@ -3,24 +3,31 @@ import { Component, OnInit, Input } from '@angular/core';
 import { BaseComponent } from '../base.component';
 
 @Component({
-  selector: 'app-page',
-  templateUrl: './page.component.html',
-  // styleUrls: ['./page.component.css']
-  encapsulation: ViewEncapsulation.None
+	selector: 'app-page',
+	templateUrl: './page.component.html',
+	// styleUrls: ['./page.component.css']
+	encapsulation: ViewEncapsulation.None,
 })
 export class PageComponent extends BaseComponent implements OnInit {
+	@Input() pageTitle?;
+	@Input() pageTitleNarrow?;
+	@Input() hasBackButton?;
 
-  @Input() pageTitle?;
-  @Input() pageTitleNarrow?;
-  @Input() hasBackButton?;
+	constructor(public injector: Injector) {
+		super(injector);
+	}
 
-  constructor(public injector: Injector) { super(injector); }
+	ngOnInit() {
+		setTimeout(() => {
+			window.scrollTo({
+				top: 0,
+				left: 0,
+				behavior: 'smooth',
+			});
+		}, 1000);
+	}
 
-  ngOnInit() {
-  }
-
-  back() {
-    window.history.back()
-  }
-
+	back() {
+		window.history.back();
+	}
 }
